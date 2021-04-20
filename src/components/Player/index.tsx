5066d1927c22bb2c0a4a043aad999942a6aac613
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-04-20 13:41:59 -0300

    player scss styles

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,12 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
+export function Player() {
+  return (
+    <div className={styles.playerContainer}>
+      <div></div>
+    </div>
+  );
+}
