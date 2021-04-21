6a14e70fa53df1a8b2390090289771d679c81630
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-04-21 10:21:56 -0300

    data formated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
+import { parse } from 'node:path';
 
 type Episode = {
   id: string;
   title: string;
   members: string;
+  published_at: string;
 };
 
 type HomeProps = {
@@ -31,6 +35,22 @@ export const getStaticProps: GetStaticProps = async () => {
       _sort: 'published_at',
       _order: 'desc',
     },
+  });
+
+  //Format data after calling the API
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      description: episode.description,
+      url: episode.filter.url,
+    };
   });
 
   return {
