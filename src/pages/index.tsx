196c508cac4afca252b5b8fc6f793cfe4954962b
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-04-21 10:12:38 -0300

    axios configured

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,37 @@
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+};
+
+type HomeProps = {
+  episodes: Episode[];
+};
+
 //SPA
 //SSR
 //SSG
 
-export default function Home(props) {
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
-      {/* <p>{JSON.stringify(props.episodes)}</p> */}
+      <p>{JSON.stringify(props.episodes)}</p>
     </div>
   );
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  });
 
   return {
     props: {
@@ -21,4 +39,4 @@ export async function getStaticProps() {
     },
     revalidate: 60 * 60 * 8,
   };
-}
+};
