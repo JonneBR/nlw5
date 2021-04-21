aba5565fdc40801dae843f65553d42fa0c6bffae
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-04-21 15:02:48 -0300

    table scss styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -76,7 +76,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
             {allEpisodes.map((episode) => {
               return (
                 <tr key={episode.id}>
-                  <td>
+                  <td style={{ width: 72 }}>
                     <Image
                       width={120}
                       height={120}
@@ -89,7 +89,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                     <a href=''>{episode.title}</a>
                   </td>
                   <td>{episode.members}</td>
-                  <td>{episode.publishedAt}</td>
+                  <td style={{ width: 100 }}>{episode.publishedAt}</td>
                   <td>{episode.duration}</td>
                   <td>
                     <button type='button'>
