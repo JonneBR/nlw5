6702f876bf9dc932859865756d52dc2175c29c1d
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-04-21 11:06:20 -0300

    configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,27 +3,47 @@ import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { parse } from 'node:path';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+
+import styles from './home.module.scss';
 
 type Episode = {
   id: string;
   title: string;
+  thumnail: string;
+  description: string;
   members: string;
+  duration: string;
+  durationAsString: string;
+  url: string;
   published_at: string;
 };
 
 type HomeProps = {
-  episodes: Episode[];
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
 };
 
 //SPA
 //SSR
 //SSG
 
-export default function Home(props: HomeProps) {
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
-    <div>
-      <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+        <ul>
+          {latestEpisodes.map((episode) => {
+            return (
+              <li>
+                <a href=''>{episode.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}></section>
     </div>
   );
 }
@@ -47,15 +67,22 @@ export const getStaticProps: GetStaticProps = async () => {
       publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
         locale: ptBR,
       }),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
       duration: Number(episode.file.duration),
       description: episode.description,
-      url: episode.filter.url,
+      url: episode.file.url,
     };
   });
 
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+
   return {
     props: {
-      episodes: data,
+      latestEpisodes,
+      allEpisodes,
     },
     revalidate: 60 * 60 * 8,
   };
