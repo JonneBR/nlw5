1fe8573d13bc98fdb685d6d93e1c1730db49b466
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-04-22 17:36:56 -0300

    context configured

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,14 +1,20 @@
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useContext } from 'react';
+import { PlayerContext } from '../../contexts/PlayerContext';
 
 import styles from './styles.module.scss';
 
 export function Player() {
+  const { episodeList, currentEpisodeIndex } = useContext(PlayerContext);
+
+  const episode = episodeList[currentEpisodeIndex];
+
   return (
     <div className={styles.playerContainer}>
       <header>
         <img src='/playing.svg' alt='Tocando agora' />
-        <strong>Tocando agora</strong>
+        <strong>Tocando agora: {episode?.title}</strong>
       </header>
 
       <div className={styles.emptyPlayer}>
