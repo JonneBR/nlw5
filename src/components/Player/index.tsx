52069b3b15a700675b328ac243a95d2cdc7f5226
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-04-23 16:13:47 -0300

    shuffle working

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -26,6 +26,7 @@ export function Player() {
     toggleLoop,
     hasNext,
     hasPrevious,
+    clearPlayerState,
   } = usePlayer();
 
   useEffect(() => {
@@ -51,6 +52,14 @@ export function Player() {
   function handleSeek(amount: number) {
     audioRef.current.currentTime = amount;
     setProgress(amount);
+  }
+
+  function handleEpisodeEnded() {
+    if (hasNext) {
+      playNext();
+    } else {
+      clearPlayerState();
+    }
   }
 
   const episode = episodeList[currentEpisodeIndex];
@@ -103,6 +112,7 @@ export function Player() {
             src={episode.url}
             ref={audioRef}
             autoPlay
+            onEnded={handleEpisodeEnded}
             loop={isLooping}
             onPlay={() => setPlayingState(true)}
             onPause={() => setPlayingState(false)}
--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -21,6 +21,7 @@ type PlayerContextData = {
   togglePlay: () => void;
   toggleShuffle: () => void;
   toggleLoop: () => void;
+  clearPlayerState: () => void;
   hasNext: boolean;
   hasPrevious: boolean;
   setPlayingState: (state: boolean) => void;
@@ -69,8 +70,13 @@ export function PlayerContextProvider({
     setIsPlaying(state);
   }
 
+  function clearPlayerState() {
+    setEpisodeList([]);
+    setCurrentEpisodeIndex(0);
+  }
+
   const hasPrevious = currentEpisodeIndex > 0;
-  const hasNext = currentEpisodeIndex + 1 < episodeList.length;
+  const hasNext = isShuffling || currentEpisodeIndex + 1 < episodeList.length;
 
   function playNext() {
     if (isShuffling) {
@@ -107,6 +113,7 @@ export function PlayerContextProvider({
         hasPrevious,
         toggleLoop,
         toggleShuffle,
+        clearPlayerState,
       }}
     >
       {children}
